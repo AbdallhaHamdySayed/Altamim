ec35c101a6f98724b3c34a8187d4951dfaca7dd6
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-10-26 13:49:44 +0300

    Init

---
+++ src/main/java/com/example/Altamim_clone/AltamimCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Altamim_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltamimCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltamimCloneApplication.class, args);
+	}
+
+}
